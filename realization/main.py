76a2130cee067ec7c5db00cec118cfcf1257f0d9
E-Kozyreva <kozyreva.k1@mail.ru>
2023-10-05 00:30:37 +0300

    add gui app

--- realization/main.py
+++ realization/main.py
@@ -1,65 +1,73 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QFont
+from PyQt6.QtCore import QSize, Qt
 import sys
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
- 
+
         self.setWindowTitle("Sorting algorithms")
-        self.setGeometry(1000, 100, 500, 700)
-        self.setFixedSize(500, 700)
-        self.setStyleSheet("background-color: #240046;")
+        self.main_layout = QHBoxLayout()
+
+        self.left, self.top, self.width, self.height = 1100, 100, 360, 600
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setFixedSize(360, 600)
+        self.setStyleSheet("background-color: #ffffff;")
         
-        # плашка с округленными краям
-        self.up = QLabel(self)
-        self.up.setStyleSheet("background-color: #3c096c; border-radius: 10px;")
-        self.up.setGeometry(0, -10, 500, 60)
+        self.time = QLabel(self)
+        self.time.setText("")
+        self.time.setFont(QFont("Arial", 10, QFont.Weight.Bold))
+        self.time.setStyleSheet("background-color: #ffffff; color: white; border-radius: 10px;")
+        self.time.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.time.move(100, -5)
+        self.time.setFixedSize(160, 30)
+
+        self.name_app = QLabel(self)
+        self.name_app.setText("Sorting algorithms")
+        self.name_app.setFont(QFont("Arial", 25, QFont.Weight.Bold))
+        self.name_app.setStyleSheet("background-color: #262626; color: white; border-radius: 20px;")
+        self.name_app.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.name_app.move(4, 4)
+        self.name_app.setFixedSize(352, 80)
+        self.name_app.lower()
         
-        # текст к верхней плашке
-        self.label = QLabel(self)
-        self.label.setText("Sorting algorithms")
-        self.label.setStyleSheet("background-color: rgba(0, 0, 0, 0); color: white;")
-        self.label.setFont(QFont("Arial", 20, QFont.Weight.Bold))
-        self.label.adjustSize()
-        self.label.move(150, 10)
+        self.end_app = QLabel(self)
+        self.end_app.setStyleSheet("background-color: #262626; border-radius: 20px;")
+        self.end_app.move(4, 516)
+        self.end_app.setFixedSize(352, 80)
         
-        # кнопка merge sort
-        self.merge_sort = QPushButton("Merge sort", self)
+        button_widget = QWidget(self)
+        button_widget.setStyleSheet("background-color: #353535;")
+        button_layout = QVBoxLayout(button_widget)
+        button_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        button_widget.setFixedSize(352, 520)
+        button_widget.move(4, 50)
+        button_widget.lower()
+        
+        self.merge_sort = QPushButton("Merge sort")
         self.merge_sort.setGeometry(150, 100, 200, 50)
-        self.merge_sort.setFont(QFont("Arial", 15, QFont.Weight.Bold))
-        self.merge_sort.setStyleSheet("background-color: #5a189a; color: white; border-radius: 10px;")
-        self.merge_sort.clicked.connect(self.merge_sort_clicked)
+        self.merge_sort.setFont(QFont("Arial", 20))
+        self.merge_sort.setStyleSheet("background-color: #262626; color: white; border-radius: 10px;")
+        self.merge_sort.setFixedSize(200, 40)
         
-        # кнопка tree sort
-        self.tree_sort = QPushButton("Tree sort", self)
-        self.tree_sort.setGeometry(150, 200, 200, 50)
-        self.tree_sort.setFont(QFont("Arial", 15, QFont.Weight.Bold))
-        self.tree_sort.setStyleSheet("background-color: #5a189a; color: white; border-radius: 10px;")
-        self.tree_sort.clicked.connect(self.tree_sort_clicked)
+        self.tree_sort = QPushButton("Tree sort")
+        self.tree_sort.setGeometry(150, 200, 200, 100)
+        self.tree_sort.setFont(QFont("Arial", 20))
+        self.tree_sort.setStyleSheet("background-color: #262626; color: white; border-radius: 10px;")
+        self.tree_sort.setFixedSize(200, 40)
         
-        # кнопка выхода 
         self.exit = QPushButton("Exit", self)
         self.exit.setGeometry(150, 600, 200, 50)
-        self.exit.setFont(QFont("Arial", 15, QFont.Weight.Bold))
-        self.exit.setStyleSheet("background-color: #ff9100; color: white; border-radius: 10px;")
-        self.exit.clicked.connect(self.close)
+        self.exit.setFont(QFont("Arial", 20))
+        self.exit.setStyleSheet("background-color: #ffffff; color: black; border-radius: 10px;")
+        self.exit.setFixedSize(200, 40)
         
+        button_layout.addWidget(self.merge_sort)
+        button_layout.addWidget(self.tree_sort)
+        button_layout.addWidget(self.exit)
         
-    def merge_sort_clicked(self):
-        print("Merge sort button clicked")
-        
-    def tree_sort_clicked(self):
-        print("Tree sort button clicked")
-        
-    def back_clicked(self):
-        print("Back button clicked")
-    
-    def exit_clicked(self):
-        print("Exit button clicked")
-        self.close()
-    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
